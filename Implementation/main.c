62bc36c4a3da5e7c74ad319cbe6e8b9fba8173dd
Matej Rástocký <rastockymatej@gmail.com>
2020-03-12 16:07:07 +0100

    refactoring, clean up, bugfix

--- Implementation/main.c
+++ Implementation/main.c
@@ -15,10 +15,12 @@ int   memory_check (void *ptr);
 void  memory_init  (void *ptr, unsigned int size);
 
 // helpers:
-void 			gen_header		(void *ptr, int size, int locked);
+void			gen_header		(void *ptr, int size, int locked);
 unsigned int 	get_block_size 	(void *ptr);
-char 			get_block_lock 	(void *ptr);
-char 			get_header_size ();
+char			get_block_lock 	(void *ptr);
+char			get_header_size ();
+int 			cant_alloc		(void *ptr, int size);
+void 			*next_block		(void *ptr);
 
 
 // START OF REGION BLOCK //
@@ -38,12 +40,11 @@ int main ()
 	memory_init(region, REGION_SIZE);
 
 
-	void *first_block = memory_alloc(5);
+	char *first_block = (char *)memory_alloc(10);
 	void *second_block = memory_alloc(10);
 
-	memory_free(first_block);
-
-	memory_alloc(2);
+	memory_alloc(40);
+	memory_alloc(20);
 
 	return 0;
 }
@@ -65,44 +66,41 @@ void memory_init (void *ptr, unsigned int size)
 
 void* memory_alloc (unsigned int size)
 {
-	printf("memory_alloc:\n");
+	printf("alloc memory block with size: %d\n", size);
 
 	// find free space
 	char* ptr = region;
 
-	while (get_block_lock(ptr))
-	{
-		if ( memory_check(ptr + get_block_size(ptr) + HEADER_SIZE) )
-		{
+	while (cant_alloc(ptr, size)) // the block is locked or too small
+		// the block is free, but too small, maybe next block is free as well, so join them
+		/* if (!get_block_lock(ptr) && !get_block_lock(ptr + get_block_size(ptr) + HEADER_SIZE)) */ 
+		/* 	gen_header(ptr, get_block_size(ptr) + get_block_size(ptr + get_block_size(ptr) + HEADER_SIZE), 0); */
+		if ( memory_check(next_block(ptr)) ) //is the new pointer still in range?
 			ptr += get_block_size(ptr) + HEADER_SIZE;
-			
-			printf("block size: %d\n", get_block_size(ptr));
-			printf("block lock: %d\n", get_block_lock(ptr));
-		}
 		else
-		{
 			return NULL;
-		}
-	}
 
 	// SPLIT IT
-	// header for allocated space
-	gen_header(ptr + size + HEADER_SIZE, get_block_size(ptr) - size, 0);
-	// header for rest
-	gen_header(ptr, size, 1);
+	if ( get_block_size(ptr) >= size + HEADER_SIZE )			 //is there enough space?
+	{
+		// header for rest
+		gen_header(ptr + HEADER_SIZE + size, get_block_size(ptr) - HEADER_SIZE - size, 0);
+		// header for allocated space
+		gen_header(ptr, size, 1);
+	}
 
-	return ptr;
+	return ptr + HEADER_SIZE;
 }
 
 int memory_free (void *valid_ptr)
 {
+	// TODO - join free blocks together
 	gen_header(valid_ptr, get_block_size(valid_ptr), 0);
+	return 1;
 }
 
 int memory_check (void *ptr)
 {
-	printf("memory_check:\n");
-
 	if ( (char *)ptr >= (char *)region &&
 		 (char *)ptr <  (char *)(region + REGION_SIZE) )
 		return 1;
@@ -113,7 +111,6 @@ int memory_check (void *ptr)
 // helpers:
 void gen_header (void *ptr, int size, int locked)
 {
-	printf("gen_header:\n");
 	int int_size  = sizeof(unsigned int);
 
 	// block length
@@ -135,3 +132,12 @@ char get_block_lock (void *ptr)
 	return *(char*)((char*)ptr + sizeof(unsigned int));
 }
 
+int cant_alloc (void *ptr, int size)
+{
+	return get_block_lock(ptr) || get_block_size(ptr) <= size + HEADER_SIZE;
+}
+
+void* next_block (void *ptr)
+{
+	return (void *)( ptr + HEADER_SIZE + get_block_size(ptr) );
+}
